dfbb1caa60d6174b7c6980374ee34af5fbe8a897
psmon <psmon@live.co.kr>
2018-11-29 00:26:51 +0900

    add React with typescript

--- front-web/src/main/java/com/webnori/psmon/cloudspring/frontweb/FrontWebApplication.java
+++ front-web/src/main/java/com/webnori/psmon/cloudspring/frontweb/FrontWebApplication.java
@@ -1,9 +1,14 @@
 package com.webnori.psmon.cloudspring.frontweb;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan
+// Using a root package also allows the @ComponentScan annotation to be used without needing to specify a basePackage attribute
 public class FrontWebApplication {
 
 	public static void main(String[] args) {
---
+++ front-web/src/main/java/com/webnori/psmon/cloudspring/frontweb/controller/GreetingController.java
@@ -0,0 +1,14 @@
+package com.webnori.psmon.cloudspring.frontweb.controller;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    @RequestMapping("/hello/{name}")
+    String hello(@PathVariable String name) {
+        return "Hello, " + name + "!";
+    }
+}
