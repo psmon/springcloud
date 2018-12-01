4c3ceb8001e714b11737be5535d46600e1ea98c9
psmon <psmon@live.co.kr>
2018-12-01 14:13:31 +0900

    유레카 추가및 pom.xml 의존트리 정리

---
+++ eureka-serviceregistry/src/main/java/com/webnori/psmon/cloudspring/eurekaserviceregistry/EurekaServiceregistryApplication.java
@@ -0,0 +1,12 @@
+package com.webnori.psmon.cloudspring.eurekaserviceregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServiceregistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServiceregistryApplication.class, args);
+	}
+}
