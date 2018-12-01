ef8ecf403065c8a5c68335a919a39ed8958ff019
psmon <psmon@live.co.kr>
2018-12-01 19:14:34 +0900

    config server 연동

--- front-web/src/main/java/com/webnori/psmon/cloudspring/frontweb/controller/GreetingController.java
+++ front-web/src/main/java/com/webnori/psmon/cloudspring/frontweb/controller/GreetingController.java
@@ -1,14 +1,27 @@
 package com.webnori.psmon.cloudspring.frontweb.controller;
 
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RestController
 public class GreetingController {
 
     @RequestMapping("/hello/{name}")
     String hello(@PathVariable String name) {
         return "Hello, " + name + "!";
+    }
+
+
+    // Config Server Test
+    @Value("${message:Hello default}")
+    private String message;
+
+    @RequestMapping("/message")
+    String getMessage() {
+        return this.message;
     }
 }
