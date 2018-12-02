86c6b65d91c8ce5db49530d87bf1bb7af3f37329
psmon <psmon@live.co.kr>
2018-12-02 23:04:24 +0900

    all api registed to ureka

---
+++ gameapi/src/main/java/com/webnori/psmon/cloudspring/gameapi/controler/GreetingController.java
@@ -0,0 +1,21 @@
+package com.webnori.psmon.cloudspring.gameapi.controler;
+
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RefreshScope
+@RestController
+public class GreetingController {
+
+
+    // Config Server Test
+    @Value("${message:Hello default}")
+    private String message;
+
+    @RequestMapping("/message")
+    String getMessage() {
+        return this.message;
+    }
+}
