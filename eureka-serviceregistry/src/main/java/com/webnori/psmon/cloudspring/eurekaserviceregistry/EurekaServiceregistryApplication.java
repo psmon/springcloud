ef855ecbe80cae24ccff8e4287d57b6f35570862
psmon <psmon@live.co.kr>
2018-12-02 18:27:23 +0900

    added : discovery service(eureka)

--- eureka-serviceregistry/src/main/java/com/webnori/psmon/cloudspring/eurekaserviceregistry/EurekaServiceregistryApplication.java
+++ eureka-serviceregistry/src/main/java/com/webnori/psmon/cloudspring/eurekaserviceregistry/EurekaServiceregistryApplication.java
@@ -2,7 +2,9 @@ package com.webnori.psmon.cloudspring.eurekaserviceregistry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class EurekaServiceregistryApplication {
 
