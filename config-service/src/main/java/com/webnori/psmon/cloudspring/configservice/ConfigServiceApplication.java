2fbc8f3481cf025dfe2c28d141269b11829e11e4
psmon <psmon@live.co.kr>
2018-12-07 03:13:09 +0900

    added : docker build

--- config-service/src/main/java/com/webnori/psmon/cloudspring/configservice/ConfigServiceApplication.java
+++ config-service/src/main/java/com/webnori/psmon/cloudspring/configservice/ConfigServiceApplication.java
@@ -2,6 +2,7 @@ package com.webnori.psmon.cloudspring.configservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
