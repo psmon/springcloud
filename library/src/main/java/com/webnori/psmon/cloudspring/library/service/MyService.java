6e663a7e225756ac9ceeb9bafe0fb32ccae180da
psmon <psmon@live.co.kr>
2018-12-09 05:23:11 +0900

    docupdate : akka

--- library/src/main/java/com/webnori/psmon/cloudspring/library/service/MyService.java
+++ library/src/main/java/com/webnori/psmon/cloudspring/library/service/MyService.java
@@ -3,6 +3,11 @@ package com.webnori.psmon.cloudspring.library.service;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.stereotype.Service;
 
+/*
+-Warn
+This is just sample. it will not be used in project. POJO is Enoght for us
+The dependency of Service or Component.  It will complicated out project
+*/
 @Service
 @EnableConfigurationProperties(ServiceProperties.class)
 public class MyService {
