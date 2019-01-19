fadf234c70aeb0be404a2a304f996660d3e7c996
psmon <psmon@live.co.kr>
2019-01-19 23:28:33 +0900

    add (akka,swagger) in datacenter

--- datacenter/src/main/java/com/webnori/psmon/cloudspring/datacenter/DatacenterApplication.java
+++ datacenter/src/main/java/com/webnori/psmon/cloudspring/datacenter/DatacenterApplication.java
@@ -3,9 +3,11 @@ package com.webnori.psmon.cloudspring.datacenter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableDiscoveryClient
 @SpringBootApplication
+@EnableSwagger2
 public class DatacenterApplication {
 
 	public static void main(String[] args) {
