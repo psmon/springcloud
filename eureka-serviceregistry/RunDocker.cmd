f7b2df58a2af204d1a81b05b9e4ef72d8a2bad48
psmon <psmon@live.co.kr>
2019-03-20 15:59:23 +0900

    update docker update doc

--- eureka-serviceregistry/RunDocker.cmd
+++ eureka-serviceregistry/RunDocker.cmd
@@ -6,4 +6,4 @@ docker build -t psmon.eureka .
 
 docker run --net devnet --ip 172.19.0.31 -it -d -p 8761:8761 -e "SPRING_PROFILES_ACTIVE=dock-local" ^
 --hostname 172.19.0.31 ^
--e "MY_HOST_IP=172.19.0.31" -e "EUREKA_URI=http://172.19.0.31:8761/eureka" --name eureka1 psmon.eureka
+-e "MY_HOST_IP=172.19.0.31" --name eureka1 psmon.eureka
